11b99858a90effa231cc6ae4bd39b6df1f745220
Muhammad Ali <malismile786@gmail.com>
2024-10-25 17:41:54 +0500

    added changes

--- src/modules/websocket/dto/create-message.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, IsOptional } from 'class-validator';
-
-export class CreateMessageDto {
-    @IsString()
-    content: string;
-
-    @IsOptional()
-    readonly type?: string;
-}
