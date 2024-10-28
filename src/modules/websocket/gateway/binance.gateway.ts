6ce27b85a225092bc9a892f0914cfc6c62079081
Muhammad Ali <malismile786@gmail.com>
2024-10-28 19:07:57 +0500

    added processing layer data

--- src/modules/websocket/gateway/binance.gateway.ts
+++ src/modules/websocket/gateway/binance.gateway.ts
@@ -19,7 +19,9 @@ export class BinanceGateway implements OnGatewayInit, OnGatewayConnection, OnGat
     private readonly binanceUrl: string = 'wss://stream.binance.com:9443/ws/btcusdt@trade';
     private isReconnecting = false;
     private lastEmissionTime: number = 0;
-    private readonly emissionInterval: number = 5000; // 5 seconds
+    private readonly emissionInterval: number = 3000;
+    private previousPrice: number | null = null;
+
 
     afterInit(server: Server): void {
         console.log('WebSocket server initialized');
@@ -43,20 +45,35 @@ export class BinanceGateway implements OnGatewayInit, OnGatewayConnection, OnGat
             console.log('Connected to Binance WebSocket API');
         });
 
+
         this.binanceWs.on('message', (data: NativeWebSocket.Data): void => {
             const currentTime = Date.now();
+
             if (currentTime - this.lastEmissionTime >= this.emissionInterval) {
                 const parsedData = JSON.parse(data.toString());
+                const currentPrice = parseFloat(parsedData.p);
+
+                const compared = this.previousPrice !== null ? currentPrice - this.previousPrice : 0;
+                const trend = this.previousPrice !== null
+                    ? (currentPrice > this.previousPrice ? 'increase' : currentPrice < this.previousPrice ? 'decrease' : 'no change')
+                    : 'no data';
+
                 const tradeData = {
                     t: parsedData.T,
-                    p: parsedData.p,
-                    s: parsedData.s
+                    s: parsedData.s,
+                    currentPrice: currentPrice,
+                    compared: compared,
+                    trend: trend
                 };
+
                 console.log('Emitting trade data:', tradeData);
                 this.server.emit('trade', tradeData);
+
                 this.lastEmissionTime = currentTime;
+                this.previousPrice = currentPrice;
             }
         });
+
 
         this.binanceWs.on('error', (error: Error): void => {
             console.error('WebSocket error:', error);
